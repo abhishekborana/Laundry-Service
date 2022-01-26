9b27af36bf4e98e5e4c4499c5e8006b2c5adf97f
imnashu07 <imnashu07@gmail.com>
2022-01-26 19:47:58 +0530

    P3Front

--- Front-End/laundry-service/src/App.js
+++ Front-End/laundry-service/src/App.js
@@ -4,6 +4,7 @@ import Homepage from './pages/Homepage';
 import {Routes , Route} from 'react-router-dom';
 import Registerpage from './pages/Registerpage';
 import Footer from './components/Footer';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route exact path = '/' element={<Homepage/>}></Route>
       <Route exact path = '/register' element={<Registerpage/>}></Route>
+      <Route exact path = '/dashboard' element={<Dashboard/>}></Route>
     </Routes>
     <Footer/>
     </>
--- Front-End/laundry-service/src/pages/Homepage.js
+++ Front-End/laundry-service/src/pages/Homepage.js
@@ -1,7 +1,43 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom'
 
 export default function Homepage() {
+    // let history = useHistory();
+    const [logindata, setLogindata] = useState({
+        email: ' ',
+        password: ' '
+    })
+
+    function changefun(e) {
+        // console.log(e.target.value)
+        setLogindata({ ...logindata, [e.target.name]: e.target.value })
+    }
+
+    async function submitfun(e) {
+        e.preventDefault()
+        console.log("inside")
+        console.log(logindata)
+        const response = await fetch("http://localhost:5000/login", {
+            method: 'POST',
+            body: JSON.stringify(logindata),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+            
+        });
+
+        const loginuserdata = await response.json()
+
+        if (loginuserdata.status) {
+            localStorage.setItem('token', loginuserdata.authtoken)
+            alert('/signinsuccefully')
+            // history('/dashboard')
+        }
+        else {
+            alert('Enter credentials again')
+        }
+    }
     return <div className='container'>
         <div className='registerpart'>
             <div className='registercontent'>
@@ -21,24 +57,24 @@ export default function Homepage() {
             <div className='signincontent'>
                 <h1 className='signintitle'>SIGN IN</h1>
                 <div className='signinform'>
-                    <form>
-                        <div className='field'>
-                            <label htmlFor="Email/phone">Mobile/Email</label>
-                            <br/>
-                            <input type="text" id='Email/phone' name="emailPhone" value='' />
+                    <form method='' action='' onSubmit={submitfun}>
+                        <div className='field reg-field'>
+                            <label htmlFor="email">Name</label>
+                            <input type="email" id='email' name="email" onChange={changefun} required />
+                            <div className='under-line'></div>
                         </div>
                         <div className='under-line'></div>
-                        <div className='field'>
+                        <div className='field reg-feild'>
                             <label htmlFor="password">Password</label>
-                            <br/>
-                            <input type="password" id='password' name="password" value=''/>
+                            <input type="password" id='password' name="password" onChange={changefun} />
                         </div>
                         <div className='under-line'></div>
                         <div className='forgot'>
                             <p>Forgot Password?</p>
                         </div>
+                        <button className='signinbutton'>Sign IN</button>
                     </form>
-                    <button className='signinbutton' type='button'>Sign IN</button>
+
                 </div>
             </div>
         </div>
